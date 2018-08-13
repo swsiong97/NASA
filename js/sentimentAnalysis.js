f18cb22b6c2bddd1ca577bb579139f927b6b799f
wei siong <swei463@gmail.com>
2018-08-13 13:13:49 +0800

    e

--- js/sentimentAnalysis.js
+++ js/sentimentAnalysis.js
@@ -37,24 +37,27 @@ function checkBtn(){
 
       },
       error: function(jqXHR,exception){
-        if (jqXHR.status === 0) {
-              alert('Not connect.\n Verify Network.');
-          } else if (jqXHR.status == 404) {
-              alert('Requested page not found. [404]');
-          } else if (jqXHR.status == 500) {
-              alert('Internal Server Error [500].');
-          } else if (exception === 'parsererror') {
-              alert('Requested JSON parse failed.');
-          } else if (exception === 'timeout') {
-              alert('Time out error.');
-          } else if (exception === 'abort') {
-              alert('Ajax request aborted.');
-          } else {
-              alert('Uncaught Error.\n' + jqXHR.responseText);
-          }
+        checkingError(jqXHR, exception);
       }
     });
 }
+}
+function checkingError(errorMessage, exception){
+  if (errorMessage.status === 0) {
+        alert('Not connect.\n Verify Network.');
+    } else if (errorMessage.status == 404) {
+        alert('Requested page not found. [404]');
+    } else if (errorMessage.status == 500) {
+        alert('Internal Server Error [500].');
+    } else if (exception == 'parsererror') {
+        alert('Requested JSON parse failed.');
+    } else if (exception == 'timeout') {
+        alert('Time out error.');
+    } else if (exception == 'abort') {
+        alert('Ajax request aborted.');
+    } else {
+        alert('Uncaught Error.\n' + errorMessage.responseText);
+    }
 }
 function clearBtn(){
       sentimentResult.value = "";
